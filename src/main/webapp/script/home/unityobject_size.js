7536a62fea333fa2cc7cb62b791a96ca703d033b
lihaijun <lihaijun@lihaijun-PC>
2016-10-08 21:35:14 +0800

    22222

--- src/main/webapp/script/home/unityobject_size.js
+++ src/main/webapp/script/home/unityobject_size.js
@@ -2,11 +2,11 @@
 	$.fn.initSize = function() {
 		var upW = $(".in_top").width();
 		$(this).width(upW * 1);
-		$(this).height(($(this).width() * 9) / 16);
+		$(this).height(($(this).width() * 1) / 2.07);
 	};
 	$.fn.fullScreenSize = function() {
 		var upW = $(".in_top").width();
 		$(this).width(upW);
-		$(this).height((upW * 9) / 16);
+		$(this).height((upW * 1) / 2.07);
 	};
 })(jQuery);
