d56e0d9a62de3b4107d31077c12e673c2be86e82
lihaijun123 <lhjzm66@126.com>
2016-10-10 16:49:43 +0800

    333333

--- src/main/java/com/focustech/focus3d/furniture/restful/FavoriteRestService.java
+++ src/main/java/com/focustech/focus3d/furniture/restful/FavoriteRestService.java
@@ -3,6 +3,7 @@ package com.focustech.focus3d.furniture.restful;
 import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -44,7 +45,7 @@ public class FavoriteRestService {
 	 * @param targetType
 	 * @return
 	 */
-	@GET
+	@POST
 	@Path("/save")
 	public String save(
 			@QueryParam("userId") String userId,
